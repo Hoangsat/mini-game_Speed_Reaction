eb89277c93e66ababda2bb13645e93cadcd78795
Hoangsat <hoangsat1@gmail.com>
2022-06-30 20:29:49 +0100

    form
    main

---
+++ Reaction/Reaction/FormReaction.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Reaction
+{
+    partial class FormReaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormReaction));
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.picture3 = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture1
+            // 
+            this.picture1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture1.Image = global::Reaction.Properties.Resources.photo_2022_05_23_22_25_17;
+            this.picture1.Location = new System.Drawing.Point(12, 12);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(203, 160);
+            this.picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture1.TabIndex = 0;
+            this.picture1.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture2.Image = global::Reaction.Properties.Resources.photo_2022_05_29_22_44_30;
+            this.picture2.Location = new System.Drawing.Point(252, 12);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(160, 160);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture2.TabIndex = 1;
+            this.picture2.TabStop = false;
+            // 
+            // picture3
+            // 
+            this.picture3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture3.Image = global::Reaction.Properties.Resources.photo_2022_06_23_10_01_48;
+            this.picture3.Location = new System.Drawing.Point(474, 12);
+            this.picture3.Name = "picture3";
+            this.picture3.Size = new System.Drawing.Size(160, 160);
+            this.picture3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture3.TabIndex = 2;
+            this.picture3.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(233, 239);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(197, 86);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FormReaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 362);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.picture3);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormReaction";
+            this.Text = "Game For Reaction";
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.PictureBox picture3;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
